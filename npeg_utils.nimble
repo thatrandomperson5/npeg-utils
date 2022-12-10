004a17c2101ddf8286bc3b799d182d42bd50f880
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2022-12-10 03:31:16 +0000

    Initial commit

---
+++ npeg_utils.nimble
@@ -0,0 +1,12 @@
+# Package
+
+version       = "0.1.0"
+author        = "thatrandomperson5"
+description   = "just some npeg utils"
+license       = "MIT"
+srcDir        = "src"
+
+
+# Dependencies
+
+requires "nim >= 1.4.8"
---
+++ src/npeg_utils/indent.nim
@@ -0,0 +1,34 @@
+import npeg
+# Inspired by the example
+var indentStack* = newSeq[int]()
+    
+
+template indent*(): int = indentStack.len
+
+template top*[T](s: seq[T]): T = 
+    if s.high == -1:
+        0
+    else:
+        s[s.high]
+
+
+
+
+grammar "ib":
+    EOL <- "\r\n" | "\n" | "\r" | !1
+    whitespace <- *Blank
+
+    Indent <- >whitespace:
+        validate len($0) > indentStack.top
+        indentStack.add len($0)
+    Dedent <- >whitespace:
+        let delStack = len($0)
+        validate delStack < indentStack.top
+        while delStack < indentStack.top:
+            discard indentStack.pop
+    Static <- >whitespace:
+        validate len($0) == indentStack.top
+
+    Line(content) <- ib.Static * content * ib.EOL
+    Block(content) <- &ib.Indent * content * &ib.Dedent
+    
